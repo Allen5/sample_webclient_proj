d0c77e39a58af76d5abe4b6a76418f590f43c55a
Allen.Wu <allenlikeu@gmail.com>
2018-04-17 17:28:42 +0800

    Allen.Wu: 完成基础前端工程框架搭建

--- webpack.config.js
+++ webpack.config.js
@@ -2,23 +2,24 @@
 //var webpack = require('webpack');
 
 const path = require('path');
+const webpack = require('webpack');
 
 /**
  * 配置项
  */
 const config = {
 
-    /**
-     * 入口文件
-     */
+    mode: 'development',
+
+    watch: true,
+
+    // 入口文件
     entry: {
         index: './source/index/index.js',
-        docs: './source/doc/index.js'
+        docs: './source/docs/index.js'
     },
 
-    /**
-     * 输出文件
-     */
+    // 输出文件
     output: {
         path: path.resolve(__dirname, "public/assets"),
         filename: "[name].bundle.js"
@@ -28,56 +29,95 @@ const config = {
      * 模块化处理css, png, jpg等资源
      * 模块化处理 es6 的js
      */
-    module: [
+    module: {
 
-        /**
-         * css样式处理
-         */
-        { 
-            test: /\.css$/, 
-            loader: "style-loader!css-loader"
-        },
+        rules: [
 
-        /**
-         * less样式处理
-         */
-        { 
-            test: /\.less$/, 
-            loader: "style-loader!css-loader!less-loader" 
-        },
+            /**
+             * css样式处理
+             */
+            { 
+                test: /\.css$/, 
+                use: [
+                    { loader: 'style-loader' },
+                    { loader: 'css-loader' }
+                ]
+            },
+
+            /**
+             * less样式处理
+             */
+            { 
+                test: /\.less$/, 
+                use: [
+                    { loader: 'style-loader' },
+                    { loader: 'css-loader' },
+                    { loader: 'less-loader' }
+                ]
+            },
 
 
-        /**
-         * js, jsx 文件中将es6的语法通过babel转换为es2015
-         * 去除node_modules等依赖库目录
-         */
-        {
-            test: /\.jsx?$/,
-            exclude: /(node_modules|bower_components)/,
-            loader: ['babel-loader'],
-            query: {
-                presets: ['es2015']
-                // 如果安装了React的话
-                // presets: ['react', 'es2015']
-            }
-        },
-        
-        /**
-         * 处理字体
-         */
-        { 
-            test: /\.(woff|woff2|eot|ttf|otf)$/i, 
-            loader: 'url-loader?limit=8192&name=[name].[ext]'
-        },
-        
-        /**
-         * 处理图片
-         */
-        { 
-            test: /\.(jpe?g|png|gif|svg)$/i, 
-            loader: 'url-loader?limit=8192&name=[name].[ext]'
-        },
-    ]
+            /**
+             * js, jsx 文件中将es6的语法通过babel转换为es2015
+             * 去除node_modules等依赖库目录
+             */
+            {
+                test: /\.jsx?$/,
+                use: [
+                    {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: ['es2015'], // presets: ['react', 'es2015']
+                        },
+                    }
+                ]
+            },
+            
+            /**
+             * 处理字体
+             */
+            { 
+                test: /\.(woff|woff2|eot|ttf|otf)$/i, 
+                use: [
+                    { 
+                        loader: 'url-loader',
+                        options: {
+                            limit: '8192',
+                            name: '[name].[ext]'
+                        }
+                    }
+                ]
+            },
+            
+            /**
+             * 处理图片
+             */
+            { 
+                test: /\.(jpe?g|png|gif|svg)$/i, 
+                use: [
+                    { 
+                        loader: 'url-loader',
+                        options: {
+                            limit: '8192',
+                            name: '[name].[ext]'
+                        }
+                    }
+                ]
+            },
+
+        ]
+
+    },
+
+    /**
+     * 开发服务
+     */
+    devServer: {
+        contentBase: path.join(__dirname, "public/"),
+        compress: true,
+        inline: true,
+        port: 4101
+    }
 
 }
 
