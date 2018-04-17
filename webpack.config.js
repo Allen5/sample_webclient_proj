d305a42d8847044851889858187a38f985bd27dc
Allen.Wu <allenlikeu@gmail.com>
2018-04-17 16:46:57 +0800

    Allen.Wu: 重新梳理工程目录

--- webpack.config.js
+++ webpack.config.js
@@ -3,8 +3,6 @@
 
 const path = require('path');
 
-import webpack from 'webpack';
-
 /**
  * 配置项
  */
@@ -22,7 +20,7 @@ const config = {
      * 输出文件
      */
     output: {
-        path: path.resolve(__dirname, "build"),
+        path: path.resolve(__dirname, "public/assets"),
         filename: "[name].bundle.js"
     },
 
@@ -33,13 +31,52 @@ const config = {
     module: [
 
         /**
-         * 文本文件处理
+         * css样式处理
          */
-        {
-            test: /\.txt$/, use: 'raw-loader'
+        { 
+            test: /\.css$/, 
+            loader: "style-loader!css-loader"
+        },
+
+        /**
+         * less样式处理
+         */
+        { 
+            test: /\.less$/, 
+            loader: "style-loader!css-loader!less-loader" 
         },
 
 
+        /**
+         * js, jsx 文件中将es6的语法通过babel转换为es2015
+         * 去除node_modules等依赖库目录
+         */
+        {
+            test: /\.jsx?$/,
+            exclude: /(node_modules|bower_components)/,
+            loader: ['babel-loader'],
+            query: {
+                presets: ['es2015']
+                // 如果安装了React的话
+                // presets: ['react', 'es2015']
+            }
+        },
+        
+        /**
+         * 处理字体
+         */
+        { 
+            test: /\.(woff|woff2|eot|ttf|otf)$/i, 
+            loader: 'url-loader?limit=8192&name=[name].[ext]'
+        },
+        
+        /**
+         * 处理图片
+         */
+        { 
+            test: /\.(jpe?g|png|gif|svg)$/i, 
+            loader: 'url-loader?limit=8192&name=[name].[ext]'
+        },
     ]
 
 }
